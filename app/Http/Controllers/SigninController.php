d6aaa42ded3f3ff8c1aeac4f42525586810eac93
unknown <andrata2908@gmail.com>
2025-01-28 15:56:14 +0700

    edit tampilan setelah login

--- app/Http/Controllers/SigninController.php
+++ app/Http/Controllers/SigninController.php
@@ -22,7 +22,7 @@ class SigninController extends Controller
         if(Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/bookfeed');
+            return redirect()->intended('/');
         }
 
         return back()->with('signinError', 'Sign in failed!');
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,38 @@
 @section('content')
 
 <section id="hero" class="content">
-    <div class="container-fluid py-5 banner-1">
-        <div class="container h-100">
-            <div class="row">
-                <div class="col-15 hero-tagline">
-                    <h1>Web Buku KnowLit</h1>
-                    <a href="{{ route('signin') }}" class="button-lg-primary try-walgita" style="text-decoration:none">Coba KnowLit</a>
-                    <img src="img/banner 1.png" alt="" class="position-absolute img-hero">
+
+    @if(Auth::check())
+        <!-- Jika sudah login -->
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header text-white" style="background-color: #AD8B73;">
+                            <h2>Selamat Datang, {{ Auth::user()->username }}!</h2>
+                        </div>
+                        <div class="card-body">
+                            <p style="color: #000;">Dengan login Anda dapat mencoba fitur BookMark kami.</p>
+                            <a href="#bookmark" class="read-more btn" style="background-color: #AD8B73; color: #fff; text-decoration: none;">Coba Sekarang</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @else
+        <!-- Jika belum login -->
+        <div class="container-fluid py-5 banner-1">
+            <div class="container h-100">
+                <div class="row">
+                    <div class="col-15 hero-tagline">
+                        <h1>Web Buku KnowLit</h1>
+                        <a href="{{ route('signin') }}" class="button-lg-primary try-walgita" style="text-decoration:none">Coba KnowLit</a>
+                        <img src="img/banner 1.png" alt="" class="position-absolute img-hero">
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 
     <div class="container-fluid py-5">
         <div class="container">
